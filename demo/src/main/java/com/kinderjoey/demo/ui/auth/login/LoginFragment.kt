f738fff6f1aa3ceb1d88cfcfda748de8d611af3e
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-08-23 19:34:23 +0700

    finish register

--- demo/src/main/java/com/kinderjoey/demo/ui/auth/login/LoginFragment.kt
+++ demo/src/main/java/com/kinderjoey/demo/ui/auth/login/LoginFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.viewbinding.library.fragment.viewBinding
+import androidx.fragment.app.commit
 import com.kinderjoey.demo.R
+import com.kinderjoey.demo.databinding.FragmentLoginBinding
+import com.kinderjoey.demo.ui.auth.register.RegisterFragment
 
 class LoginFragment : Fragment() {
+
+    private val loginBinding by viewBinding<FragmentLoginBinding>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +22,18 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         requireActivity().window.statusBarColor = View.GONE
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginBinding.tvRegister.setOnClickListener {
+            val mFragmentManager = parentFragmentManager
+            val mRegisterFragment = RegisterFragment()
+
+            mFragmentManager.commit {
+                replace(R.id.auth_container, mRegisterFragment, RegisterFragment::class.java.simpleName)
+            }
+        }
     }
 }
